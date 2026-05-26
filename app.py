489fe13fd9d997ac79c573869dd0b930764c9949
Хасбулат <gucigovhasbulat9@gmail.com>
2026-05-26 17:03:07 +0300

    realise add_task

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-
+from todo.routes import task_bp
 app = Flask(__name__)
 
+app.register_blueprint(task_bp)
 
 
 @app.route('/')
 def main():
-    return 'Homepage'
+    return '<h1>Homepage<h1>'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
--- todo/routes.py
+++ todo/routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 
 task_bp = Blueprint('tasks', __name__, template_folder='templates')
 
 tasks_db = [
     {'id': 1, 'title': 'Buy a bread', 'description': 'before shop closed'},
-    {'id': 1, 'title': 'do flip', 'description': 'just do it'},
+    {'id': 1, 'title': 'do flip', 'description': 'just do it'}, 
     {'id': 1, 'title': 'training', 'description': 'HELL YEAH'}
 ]
 
@@ -24,3 +24,19 @@ def get_all_tasks(id):
     return render_template('task.html', tasks_two=task_two)
 
 
+
+@task_bp.route('/add', methods=['GET', 'post'])
+def  add_tasks():
+    if request.method == 'post':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        tasks_db.append({'id': len(tasks_db)+1, 'title': title, 'description': description})
+        return redirect(url_for('tasks.get_all_tasks'))
+    return render_template('add_task.html')
+
+
+
+@task_bp.route('/delete/<int:id>', methods=['post'])
+def delete_task(id):
+    print(id)
+    return redirect(url_for('tasks'))
